8cff4061326efa3caf7f4e04422b1337edf9d38d
t.akasaki <t.akasaki.csharp@gmail.com>
2023-07-22 20:27:47 +0900

    feat: :tada: 初回コミット

---
+++ src/main/java/jp/artan/teleporters/SimpleTeleportersReloaded.java
@@ -0,0 +1,45 @@
+package jp.artan.teleporters;
+
+import com.mojang.logging.LogUtils;
+import jp.artan.repack.registrate.util.nullness.NonNullSupplier;
+import jp.artan.teleporters.data.SimpleTeleportersReloadedRegistrate;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SimpleTeleportersReloaded.MOD_ID)
+public class SimpleTeleportersReloaded {
+    public static final String MOD_ID = "teleporters";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private static final NonNullSupplier<SimpleTeleportersReloadedRegistrate> REGISTRATE = SimpleTeleportersReloadedRegistrate.lazy(MOD_ID);
+
+    public SimpleTeleportersReloaded() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::registerProviders);
+    }
+
+    public static ResourceLocation getResource(String name) {
+        return new ResourceLocation(MOD_ID, name);
+    }
+
+    public static SimpleTeleportersReloadedRegistrate registrate() {
+        return REGISTRATE.get();
+    }
+
+
+    public void commonSetup(FMLCommonSetupEvent evt) {
+    }
+
+    void registerProviders(GatherDataEvent event) {
+    }
+}
