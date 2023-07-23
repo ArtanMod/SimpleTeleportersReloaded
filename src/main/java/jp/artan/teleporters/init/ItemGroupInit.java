c8ed151d6cb0618797805cb288ee4234ce05e119
t.akasaki <t.akasaki.csharp@gmail.com>
2023-07-23 11:44:22 +0900

    feat: :sparkles: クリエイティブタブのアイコンをテレポーターに変更

--- src/main/java/jp/artan/teleporters/init/ItemGroupInit.java
+++ src/main/java/jp/artan/teleporters/init/ItemGroupInit.java
@@ -9,7 +9,7 @@ import net.minecraft.world.item.Items;
 public class ItemGroupInit {
     private static final SimpleTeleportersReloadedRegistrate REGISTRATE = SimpleTeleportersReloaded.registrate();
 
-    public static final CreativeTab DEEP_MOB_LEARNING_RELOADED = REGISTRATE.tab("simple_teleporters_reloaded", () -> new ItemStack(Items.APPLE))
+    public static final CreativeTab DEEP_MOB_LEARNING_RELOADED = REGISTRATE.tab("simple_teleporters_reloaded", () -> new ItemStack(BlockInit.ENDER_CRYSTAL.get()))
             .lang("SimpleTeleporters: Reloaded")
             .register();
 
